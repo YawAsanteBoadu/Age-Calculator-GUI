b6aa86712b1998b9fa9f3d89e753a73846885dc3
Yaw Asante Boadu <yawasante95@yahoo.com>
2025-08-01 05:00:20 +0000

    fix: correct script inclusion and improve age calculation logic

--- main.js
+++ main.js
@@ -9,4 +9,10 @@ function calculateAge (){
         months--;
         days += new Date(now.getFullYear(), now.getMonth(), 0).getDate();
     }
+
+    if(months < 0){
+        years--;
+        months += 12;
+    }
+    document.getElementById("result").textContent = `You are ${years}years, ${months}months, and ${days}days old`
 }
